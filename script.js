48050ac29156599f997c75ddf904184dfc9402c0
RichGuilherme <richard.guilhermeAS@hotmail.com>
2022-10-25 23:01:14 -0300

    versão 2.0

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+function submit_fomulario() {
 const buttonInicial = document.querySelector(".buttonInicial")
 
-function submit() {
     buttonInicial.classList.add("animationButton")
+    //timeout para ter um deley quando o botão inicial for retirado, ter um tempo para animação ser ser executada.
+    setTimeout(() => {
+        buttonInicial.setAttribute("hidden", "")
+    }, 1000);
+
+
+    let fomulario = document.querySelector(".content")
+
+    setInterval(() => {
+ 
+        fomulario.style.display = "flex"
+        document.body.style = "backdrop-filter: blur(5px)"
+    }, 1500)
+    
 }
 
+function submit(){}
 
+
+// x quando logar fazer o formulário ir para baixo
